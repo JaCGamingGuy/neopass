2fb67a071104dfa9992298ddf0541541d924fced
JaCGamingGuy <jaccrafting@gmail.com>
2024-08-30 02:08:47 -0400

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -240,7 +240,7 @@ def password_manager_menu():
             display_info()
             Prompt.ask("\nPress Enter to continue...")
 
-        elif option == '7, exit':
+        elif option == '7':
             clear_screen()  # Clear the screen before exiting
             console.print("[bold cyan]Exiting Password Manager...[/bold cyan]")
             break
